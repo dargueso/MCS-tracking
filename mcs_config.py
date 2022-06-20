711dbea99f07f21a159e6e50728ab0498f9d983d
Daniel Argüeso <d.argueso@unsw.edu.au>
2022-06-20 15:56:45 +0200

    Update mcs_config.py

--- mcs_config.py
+++ mcs_config.py
@@ -1,6 +1,8 @@
 
-#path_in = "/vg6/dargueso-NO-BKUP/postprocessed/unified/EPICC/EPICC_2km_ERA5_HVC_GWD/"
-path_in = "/Users/daniel/Scripts_local/MCS-tracking"
+
+path_in = "/vg6/dargueso-NO-BKUP/postprocessed/unified/EPICC/EPICC_2km_ERA5_CMIP6anom_HVC_GWD/"
+#path_in = "/Users/daniel/Scripts/MCS-tracking"
+
 ## MCS config
 DT = 1 # time step of data in hours
 Variables = ["PR", "Tb"]
@@ -10,18 +12,18 @@ Variables = ["PR", "Tb"]
 smooth_sigma_pr = 0 # Gaussion std for precipitation smoothing
 thres_pr = 5  # precipitation threshold [mm/h]
 min_time_pr= 3  # minum lifetime of PR feature in hours
-min_area_pr = 1000  # minimum area of precipitation feature in km2
+min_area_pr = 500  # minimum area of precipitation feature in km2
 
 # Brightness temperature (Tb) tracking setup
 smooth_sigma_bt = 0  # Gaussion std for Tb smoothing
 thres_bt = 241  # minimum Tb of cloud shield
 min_time_bt = 5  # minium lifetime of cloud shield in hours
-min_area_bt = 10000  # minimum area of cloud shield in km2
+min_area_bt = 1000  # minimum area of cloud shield in km2
 
 # MCs detection
 MCS_min_area = min_area_pr  # minimum area of MCS precipitation object in km2
-MCS_thres_pr = 10  # minimum max precipitation in mm/h
-MCS_thres_peak_pr = 10  # Minimum lifetime peak of MCS precipitation
+MCS_thres_pr = 5  # minimum max precipitation in mm/h
+MCS_thres_peak_pr = 15  # Minimum lifetime peak of MCS precipitation
 MCS_thres_bt = 225  # minimum brightness temperature
 MCS_min_area_bt = min_area_bt  # min cloud area size in km2
 MCS_min_time = 5  # minimum lifetime of MCS
